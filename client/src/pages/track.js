2c19518ea87c3653e49eb64be3e4cfce7478fcab
Nigma Silmi <sn.silmi@gmail.com>
2022-07-09 15:18:52 -0400

    useQuery for querying a specific track

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,9 +1,43 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
+
+const GET_TRACK = gql`
+  query getTrack($trackId: ID!) {
+    track(id: $trackId) {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+      numberOfViews
+      modules {
+        id
+        title
+        length
+      }
+      description
+    }
+  }
+`;
 
 const Track = ({ trackId }) => {
-  return <Layout>Track</Layout>;
+  const { loading, error, data } = useQuery(GET_TRACK, {
+    variables: { trackId },
+  });
+  return (
+    <Layout>
+      <QueryResult error={error} loading={loading} data={data}>
+        <TrackDetail track={data?.track} />
+      </QueryResult>
+    </Layout>
+  );
 };
 
 export default Track;
